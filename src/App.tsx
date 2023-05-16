d27bb8526dd01119f52c2d52dc7c7241b3cea122
Giorgi Silagadze <pele@Giorgis-MacBook-Pro.local>
2023-05-16 21:40:17 +0400

    style: create global styles

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,18 @@ import Technology from "./pages/Technology";
 import Planet from "./pages/Planet";
 import CrewMember from "./pages/CrewMember";
 import Vehicle from "./pages/Vehicle";
+import { Helmet } from "react-helmet";
+import { GlobalStyled } from "./styled-components/Global.Styled";
 
 function App() {
 
   return (
     <>
+    <Helmet>
+    <link href="https://fonts.googleapis.com/css2?family=Barlow&family=Barlow+Condensed:wght@400;700&family=Bellefair&display=swap" rel="stylesheet" />
+    <title>Space-tourism-website</title>
+    </Helmet>
+    <GlobalStyled />
     <Router>
       <Routes>
         <Route path="/" element={<Home />}/>
---
+++ src/styled-components/Global.Styled.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from "styled-components"
+
+
+export const GlobalStyled = createGlobalStyle`
+
+    * {
+        margin: 0px;
+        padding: 0px;
+        box-sizing: border-box;
+    }
+
+    body {
+        background-image: url("./assets/home/background-home-mobile.jpg");
+        background-repeat: no-repeat;
+    }
+`
+
