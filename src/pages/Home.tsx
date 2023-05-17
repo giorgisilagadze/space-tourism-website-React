d2bb64e0f5d7b906b4a33a8e185026aa709e39c6
Giorgi Silagadze <pele@Giorgis-MacBook-Pro.local>
2023-05-17 20:25:50 +0400

    feat: home-page is done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,26 @@
-
+import {
+  StyledHome,
+  HomeP,
+  HomeTitle,
+  HomeTxt,
+  Explore,
+} from "../styled-components/Home.Styled";
+import { StyledLink } from "../styled-components/Header.Styled";
 
 export default function Home() {
-    return <div>home</div>
+  return (
+    <StyledHome>
+      <HomeP>SO, YOU WANT TO TRAVEL TO</HomeP>
+      <HomeTitle>SPACE</HomeTitle>
+      <HomeTxt>
+        Let’s face it; if you want to go to space, you might as well genuinely
+        go to outer space and not hover kind of on the edge of it. Well sit
+        back, and relax because we’ll give you a truly out of this world
+        experience!
+      </HomeTxt>
+      <StyledLink to="/Destination">
+        <Explore>EXPLORE</Explore>
+      </StyledLink>
+    </StyledHome>
+  );
 }
---
+++ src/styled-components/Home.Styled.ts
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const StyledHome = styled.div`
+    margin-top: 48px;
+`
+
+export const HomeP = styled.p`
+    font-family: 'Barlow Condensed', sans-serif;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 19.2px;
+    letter-spacing: 2.7px;
+    text-align: center;
+    color: #D0D6F9;
+`
+
+export const HomeTitle = styled.h1`
+    font-family: 'Bellefair', serif;
+    font-weight: 400;
+    font-size: 80px;
+    line-height: 100px;
+    text-align: center;
+    margin-top: 16px;
+    color: white;
+
+`
+
+export const HomeTxt = styled.p`
+    font-family: 'Barlow', sans-serif;
+    max-width: 327px;
+    font-weight: 400;
+    font-size: 15px;
+    line-height: 25px;
+    text-align: center;
+    margin: 16px auto 0px;
+    color: #D0D6F9;
+`
+
+export const Explore = styled.div`
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Bellefair', serif;
+    font-weight: 400;
+    font-size: 20px;
+    line-height: 22.92px;
+    letter-spacing: 1.25px;
+    color: #0B0D17;
+    margin: 81px auto 0px;
+`
